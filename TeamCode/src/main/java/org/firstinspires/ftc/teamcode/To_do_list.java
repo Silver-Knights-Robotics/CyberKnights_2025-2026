a05733fca02e12b227cb8d2df42e852bb1904426
Starcart23 <westermandamien151@gmail.com>
2025-11-22 09:37:03 -0700

    Added a to do list, and MAYBE the right code for the sensor

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/To_do_list.java
@@ -0,0 +1,12 @@
+package org.firstinspires.ftc.teamcode;
+
+public class To_do_list {
+}
+/*
+These things are only for CODERS let the builders build :)
+1. Add another motor for the cannon
+2. Try testing the robot when it is done
+3. Add Sensor Code (Did that yo :D)
+4. 
+5.
+ */
