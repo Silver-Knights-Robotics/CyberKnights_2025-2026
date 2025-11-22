f5757a59c3c7ac42f247ea3e0dbdd20114dec294
Starcart23 <westermandamien151@gmail.com>
2025-11-22 09:59:40 -0700

    Update To_do_list.java

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/To_do_list.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/To_do_list.java
@@ -7,6 +7,6 @@ These things are only for CODERS let the builders build :)
 1. Add another motor for the cannon
 2. Try testing the robot when it is done
 3. Add Sensor Code (Did that yo :D)
-4. 
+4.
 5.
  */
