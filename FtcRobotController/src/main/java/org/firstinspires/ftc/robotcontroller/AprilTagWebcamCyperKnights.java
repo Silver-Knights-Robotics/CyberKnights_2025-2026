90067090a6201a8c7be90dabcc95ad4a409969dd
Starcart23 <westermandamien151@gmail.com>
2025-12-13 10:13:13 -0700

    idk man let me cry twin 🥲

---
+++ FtcRobotController/src/main/java/org/firstinspires/ftc/robotcontroller/AprilTagWebcamCyperKnights.java
@@ -0,0 +1,19 @@
+package org.firstinspires.ftc.robotcontroller;
+
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+
+public class AprilTagWebcamCyperKnights extends OpMode {
+
+    AprilTagWebcam aprilTagWebcam = new AprilTagWebcam();
+
+    @Override
+    public void init() {
+
+
+    }
+
+    @Override
+    public void loop() {
+
+    }
+}
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/mechanisms/AprilTagWebcam.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/mechanisms/AprilTagWebcam.java
@@ -16,6 +16,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class AprilTagWebcam {
+
     private AprilTagProcessor aprilTagProcessor;
     private VisionPortal visionPortal;
 
@@ -23,9 +24,8 @@ public class AprilTagWebcam {
 
     private Telemetry telemetry;
 
-    public void init(HardwareMap hwMap, Telemetry telementry) {
-        this.telemetry = telementry;
-
+    public void init(HardwareMap hwMap, Telemetry telemetry) {
+        this.telemetry = telemetry;
 
         aprilTagProcessor = new AprilTagProcessor.Builder()
                 .setDrawTagID(true)
@@ -41,6 +41,7 @@ public class AprilTagWebcam {
         builder.addProcessor(aprilTagProcessor);
 
         visionPortal = builder.build();
+
     }
 
     public void update() {
@@ -52,17 +53,29 @@ public class AprilTagWebcam {
     }
 
     public void displayDetectionTelemetry(AprilTagDetection detectedId) {
-        public AprilTagDetection getTagBySpecificId(int id) {
-            if (detectedId == null) {return;}
-            for (AprilTagDetection detection : detectedTags) {
+        if (detectedId == null); {return;}
+
+        if (detectedId.metadata != null) {
+            telemetry.addLine(String.format("\n==== (ID %d) %s", detectedId.id, detectedId.metadata.name));
+            telemetry.addLine(String.format("XYZ %6.1f %6.1f %6.1f  (inch)", detectedId.ftcPose.x, detectedId.ftcPose.y, detectedId.ftcPose.z));
+            telemetry.addLine(String.format("PRY %6.1f %6.1f %6.1f  (deg)", detectedId.ftcPose.pitch, detectedId.ftcPose.roll, detectedId.ftcPose.yaw));
+            telemetry.addLine(String.format("RBE %6.1f %6.1f %6.1f  (inch, deg, deg)", detectedId.ftcPose.range, detectedId.ftcPose.bearing, detectedId.ftcPose.elevation));
+        } else {
+            telemetry.addLine(String.format("\n==== (ID %d) Unknown", detectedId.id));
+            telemetry.addLine(String.format("Center %6.0f %6.0f   (pixels)", detectedId.center.x, detectedId.center.y));
+        }
+    }
+
+
+
+    public AprilTagDetection getTagBySpecificId(int id) {
+        for (AprilTagDetection detection : detectedTags) {
             if (detection.id == id){
                 return detection;
-
             }
         }
         return null;
     }
-
     public void stop() {
         if (visionPortal != null) {
             visionPortal.close();
