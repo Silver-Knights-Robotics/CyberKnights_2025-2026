cde52cef1ba2c22595319bc218c9419929f87f0b
Starcart23 <westermandamien151@gmail.com>
2025-12-13 09:45:00 -0700

    Update AprilTagWebcam.java

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/mechanisms/AprilTagWebcam.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/mechanisms/AprilTagWebcam.java
@@ -1,8 +1,13 @@
 package org.firstinspires.ftc.teamcode.mechanisms;
 
+import android.util.Size;
+
 import com.qualcomm.robotcore.hardware.HardwareMap;
 
 import org.firstinspires.ftc.robotcore.external.Telemetry;
+import org.firstinspires.ftc.robotcore.external.hardware.camera.WebcamName;
+import org.firstinspires.ftc.robotcore.external.navigation.AngleUnit;
+import org.firstinspires.ftc.robotcore.external.navigation.DistanceUnit;
 import org.firstinspires.ftc.vision.VisionPortal;
 import org.firstinspires.ftc.vision.apriltag.AprilTagDetection;
 import org.firstinspires.ftc.vision.apriltag.AprilTagProcessor;
@@ -19,6 +24,48 @@ public class AprilTagWebcam {
     private Telemetry telemetry;
 
     public void init(HardwareMap hwMap, Telemetry telementry) {
+        this.telemetry = telementry;
 
+
+        aprilTagProcessor = new AprilTagProcessor.Builder()
+                .setDrawTagID(true)
+                .setDrawTagOutline(true)
+                .setDrawAxes(true)
+                .setDrawCubeProjection(true)
+                .setOutputUnits(DistanceUnit.CM, AngleUnit.DEGREES)
+                .build();
+
+        VisionPortal.Builder builder = new VisionPortal.Builder();
+        builder.setCamera(hwMap.get(WebcamName.class, "Webcam 1"));
+        builder.setCameraResolution(new Size(640, 480));
+        builder.addProcessor(aprilTagProcessor);
+
+        visionPortal = builder.build();
+    }
+
+    public void update() {
+        detectedTags = aprilTagProcessor.getDetections();
+    }
+
+    public List<AprilTagDetection> getDetectedTags() {
+        return detectedTags;
+    }
+
+    public void displayDetectionTelemetry(AprilTagDetection detectedId) {
+        public AprilTagDetection getTagBySpecificId(int id) {
+            if (detectedId == null) {return;}
+            for (AprilTagDetection detection : detectedTags) {
+            if (detection.id == id){
+                return detection;
+
+            }
+        }
+        return null;
+    }
+
+    public void stop() {
+        if (visionPortal != null) {
+            visionPortal.close();
+        }
     }
 }
